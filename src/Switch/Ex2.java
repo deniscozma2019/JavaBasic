8649915cbd459f9e289bd9a8dc8e47566388d4c0
deniscozma2019 <denis.cozma2015@gmail.com>
2022-06-27 12:51:16 +0300

    Switch

---
+++ src/Switch/Ex2.java
@@ -0,0 +1,17 @@
+package Switch;
+
+public class Ex2 {
+    public static void main(String[] args){
+        int day = 4;
+        switch (day){
+            case 1:
+                System.out.println("Saturday");
+                break;
+            case 2 :
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("weekend");
+        }
+    }
+}
